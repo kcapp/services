5e3bb20d5b0912180a56d99cc3768599190ea7fc
Thord Setsaas <t.setsaas@sportradar.com>
2025-03-04 15:43:41 +0100

    Don't insert data no longer needed

--- installer.sh
+++ installer.sh
@@ -78,7 +78,6 @@ required node
 required npm
 required docker
 #required docker-compose
-#required mysqlsh
 required goose
 print_result
 
@@ -114,12 +113,6 @@ if [ "$INSTALL" = true ] ; then
     echo "Running database migrations"
     cd migrations/
     goose mysql "kcapp:abcd1234@tcp(localhost:3366)/kcapp?parseTime=true" up
-
-    # Insert some required data
-    #mysqlsh -u kcapp -pabcd1234 localhost:3366/kcapp --sql << EOF
-    #INSERT INTO office(id, \`name\`, is_active) VALUES (1, 'Test', 1);
-    #EOF
-
 else
     git_pull $INSTALL_DIRECTORY/database
     db_change=$(echo $?)
